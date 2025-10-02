9145678e52eeaaef230e2b975cf9f6815c92a8c6
jonghoon_lee <adjh54ir@gmail.com>
2025-10-02 23:21:33 +0900

    1.2.1 버전 패치

--- src/navigation/AppLayout.tsx
+++ src/navigation/AppLayout.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { View, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, Platform, Dimensions } from 'react-native';
 import { NavigationContainer, NavigationContainerRef } from '@react-navigation/native';
 import { Paths } from '@/navigation/conf/Paths';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -10,121 +10,123 @@ import DeviceInfo from 'react-native-device-info';
 import LevelPlayBannerAd from '@/screens/common/ads/levelplay/LevelPlayBannerAd';
 
 const AD_ALLOWED_ROUTES = [
-    Paths.TODAY_QUIZ,
-    Paths.PROVERB_LIST,
-    Paths.HOME,
-    Paths.SETTING,
-    Paths.MY_SCORE,
-    // 필요하면 추가
+	Paths.TODAY_QUIZ,
+	Paths.PROVERB_LIST,
+	Paths.HOME,
+	Paths.SETTING,
+	Paths.MY_SCORE,
+	// 필요하면 추가
 ];
 
-const SDK_KEY = 'YOUR_SDK_KEY';
-const BANNER_AD_UNIT_ID = 'YOUR_BANNER_AD_UNIT_ID';
+const designHeight = 812;
+
+const { height: screenHeight } = Dimensions.get('window');
 
 const AppLayout = () => {
-    const navigationRef = useRef<NavigationContainerRef<any>>(null);
-    const [currentRoute, setCurrentRoute] = useState<string>(Paths.HOME);
+	const navigationRef = useRef<NavigationContainerRef<any>>(null);
+	const [currentRoute, setCurrentRoute] = useState<string>(Paths.HOME);
 
-    const shouldShowAd = useMemo(() => AD_ALLOWED_ROUTES.includes(currentRoute), [currentRoute]);
+	const shouldShowAd = useMemo(() => AD_ALLOWED_ROUTES.includes(currentRoute), [currentRoute]);
 
-    // useEffect(() => {
-    // 	AppLovinMAX.initialize(SDK_KEY)
-    // 		.then((config: Configuration) => {
-    // 			console.log('✅ AppLovin MAX Initialized', config);
-    // 			// 테스트 모드 ON (테스트 광고 강제 노출)
-    // 			// AppLovinMAX.showMediationDebugger(); // ✅ 강제로 광고 네트워크/테스트 광고 확인
-    // 		})
-    // 		.catch((error) => {
-    // 			console.error('AppLovin MAX 초기화 실패:', error);
-    // 		});
-    // }, []);
+	// useEffect(() => {
+	// 	AppLovinMAX.initialize(SDK_KEY)
+	// 		.then((config: Configuration) => {
+	// 			console.log('✅ AppLovin MAX Initialized', config);
+	// 			// 테스트 모드 ON (테스트 광고 강제 노출)
+	// 			// AppLovinMAX.showMediationDebugger(); // ✅ 강제로 광고 네트워크/테스트 광고 확인
+	// 		})
+	// 		.catch((error) => {
+	// 			console.error('AppLovin MAX 초기화 실패:', error);
+	// 		});
+	// }, []);
 
-    // ✅ 라우트별 배경색 지정
-    const backgroundColor = useMemo(() => {
-        switch (currentRoute) {
-            case Paths.SETTING:
-                return '#f9f9f9';
-            case Paths.MY_SCORE:
-                return '#ffffff';
-            case Paths.TODAY_QUIZ:
-                return '#f5f5f5';
-            case Paths.PROVERB_LIST:
-                return '#f8f9fa';
-            default:
-                return '#ffffff'; // 기본값
-        }
-    }, [currentRoute]);
+	// ✅ 라우트별 배경색 지정
+	const backgroundColor = useMemo(() => {
+		switch (currentRoute) {
+			case Paths.SETTING:
+				return '#f9f9f9';
+			case Paths.MY_SCORE:
+				return '#ffffff';
+			case Paths.TODAY_QUIZ:
+				return '#f5f5f5';
+			case Paths.PROVERB_LIST:
+				return '#f8f9fa';
+			default:
+				return '#ffffff'; // 기본값
+		}
+	}, [currentRoute]);
 
-    return (
-        <NavigationContainer
-            ref={navigationRef}
-            onReady={() => {
-                setCurrentRoute(navigationRef.current?.getCurrentRoute()?.name || '');
-            }}
-            onStateChange={() => {
-                const routeName = navigationRef.current?.getCurrentRoute()?.name;
-                if (routeName) {
-                    setCurrentRoute(routeName);
-                }
-            }}>
-            <SafeAreaView style={[styles.safeArea, { backgroundColor }]} edges={shouldShowAd ? ['top'] : []}>
-
-                <View style={styles.container}>
-                    <View style={[styles.adWrapperAbsolute, !shouldShowAd && { height: 0, opacity: 0 }]}>
-                        <LevelPlayBannerAd visible={shouldShowAd} paramMarginTop={0} paramMarginBottom={0} />
-                    </View>
-                    {shouldShowAd ? (
-                        <View
-                            style={{
-                                marginBottom: DeviceInfo.isTablet() // ✅ 테블릿이면 40
-                                    ? scaleHeight(40)
-                                    : Platform.OS === 'android' // ✅ 그 외 Android면 30
-                                        ? scaleHeight(30)
-                                        : scaleHeight(0), // ✅ iOS 기본값
-                            }}
-                        />
-                    ) : (
-                        <></>
-                    )}
-                    <View
-                        style={[
-                            styles.navigatorWrapper,
-                            shouldShowAd && { paddingTop: Platform.OS === 'android' ? scaleHeight(50) : scaleHeight(25) },
-                            { backgroundColor },
-                        ]}>
-                        <StackNavigator />
-                    </View>
-                </View>
-            </SafeAreaView>
-        </NavigationContainer>
-    );
+	return (
+		<NavigationContainer
+			ref={navigationRef}
+			onReady={() => {
+				setCurrentRoute(navigationRef.current?.getCurrentRoute()?.name || '');
+			}}
+			onStateChange={() => {
+				const routeName = navigationRef.current?.getCurrentRoute()?.name;
+				if (routeName) {
+					setCurrentRoute(routeName);
+				}
+			}}>
+			<SafeAreaView style={[styles.safeArea, { backgroundColor }]} edges={shouldShowAd ? ['top'] : []}>
+				<View style={styles.container}>
+					<View style={[styles.adWrapperAbsolute, !shouldShowAd && { height: 0, opacity: 0 }]}>
+						<LevelPlayBannerAd visible={shouldShowAd} paramMarginTop={0} paramMarginBottom={0} />
+					</View>
+					{shouldShowAd ? (
+						<View
+							style={{
+								paddingTop: DeviceInfo.isTablet()
+									? scaleHeight(60) // 배너 높이 + 여백
+									: Platform.OS === 'android'
+										? scaleHeight(50)
+										: screenHeight < designHeight
+											? 40 // ✅ 작은 화면일 때 여유 더 줌
+											: 0,
+							}}
+						/>
+					) : (
+						<></>
+					)}
+					<View
+						style={[
+							styles.navigatorWrapper,
+							shouldShowAd && { paddingTop: Platform.OS === 'android' ? scaleHeight(50) : scaleHeight(25) },
+							{ backgroundColor },
+						]}>
+						<StackNavigator />
+					</View>
+				</View>
+			</SafeAreaView>
+		</NavigationContainer>
+	);
 };
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    adWrapperAbsolute: {
-        position: 'absolute',
-        top: Platform.OS === 'android' ? scaleHeight(20) : scaleHeight(6),
-        left: 0,
-        right: 0,
-        zIndex: 10,
-        paddingVertical: scaleHeight(4),
-        marginHorizontal: scaleWidth(16),
-        alignItems: 'center',
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 6,
-    },
-    container: {
-        flex: 1,
-        backgroundColor: 'transparent',
-    },
-    navigatorWrapper: {
-        flex: 1,
-    },
+	safeArea: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	adWrapperAbsolute: {
+		position: 'absolute',
+		top: Platform.OS === 'android' ? scaleHeight(20) : scaleHeight(6),
+		left: 0,
+		right: 0,
+		zIndex: 10,
+		paddingVertical: scaleHeight(4),
+		marginHorizontal: scaleWidth(16),
+		alignItems: 'center',
+		borderWidth: 1,
+		borderColor: '#ccc',
+		borderRadius: 6,
+	},
+	container: {
+		flex: 1,
+		backgroundColor: 'transparent',
+	},
+	navigatorWrapper: {
+		flex: 1,
+	},
 });
 
 export default AppLayout;
